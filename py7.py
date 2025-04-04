f18dfd6ef7da0093bda0df4e34415717d56109e4
William F. Gilreath <will.gilreath@outlook.com>
2025-04-03 21:23:41 -0700

    Add files via upload
    Updated install modules if not found, and output format/style.

--- py7.py
+++ py7.py
@@ -1,66 +1,70 @@
 #!/usr/bin/env python3
 #
-#
 
 """
+File: py7.py "PI-seven" or π-7
 
-Name: py7.py "PI-seven" or π-7
+Author: William F. Gilreath (will@wfgilreath.xyz)
 
-Description: This Python app uses seven Python lint tools to check Python source code. The seven tools are:
+Date: 03-30-2025
 
-                bandit    - Bandit is a tool designed to find common security issues in Python source code.
-                mccabe    - Check McCabe complexity in Python source code.
-                mypy      - Mypy is a static type checker for Python source code.
-                pyflakes  - Pyflakes checks source files for errors in Python source code.
-                pylint    - Pylint analyses and checks for errors in Python source code.
-                ruff      - Ruff is fast Python check and lint for Python source code.
-                vulture   - Vulture detects unused "dead" code in Python source code.
+Description: This Python app uses seven Python lint tools to check Python
+source code. The seven tools are:
 
-Copyright © 2023 William F. Gilreath (will@wfgilreath.xyz) All Rights Reserved.
+    bandit    - Bandit is a tool designed to find common security
+                issues in Python source code.
+    mccabe    - Check McCabe complexity in Python source code.
+    mypy      - Mypy is a static type checker for Python source code.
+    pyflakes  - Pyflakes checks source files for errors in Python
+                source code.
+    pylint    - Pylint analyses and checks for errors in Python source code.
+    ruff      - Ruff is fast Python check and lint for Python source code.
+    vulture   - Vulture detects unused "dead" code in Python source code.
 
-License: This program is free software: you can redistribute it and/or modify it under the terms of the
-GNU General Public License as published by the Free Software Foundation, either version 3 of the License,
-or (at your option) any later version.
+Copyright © 2025 William F. Gilreath. All Rights Reserved.
 
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+License: This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
 
-You should have received a copy of the GNU General Public License along with this program.
-If not, see <https://www.gnu.org/licenses/>.
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <https://www.gnu.org/licenses/>.
 
 """
 
 import importlib.util
 import os
 import sys
-import subprocess
+from runpy import run_module
 from subprocess import PIPE, run
 
 __author__ = "William F. Gilreath"
-__copyright__ = "Copyright 2023"
+__copyright__ = "Copyright 2025"
 __credits__ = ["William F. Gilreath"]
 __license__ = "GPL"
 __version__ = "v3.0"
 __maintainer__ = "William F. Gilreath"
 __email__ = "william.f.gilreath@gmail.com"
-__release__ = "v 1.1"
+__release__ = "v 1.3.1"
 
 MCCABE_MIN = '3'  # mccabe cyclotomic complexity threshold for error
 
 # required external test modules used by py7
-PY_MOD_LIST = ['bandit',
-               'mccabe',
-               'mypy',
-               'pyflakes',
-               'pylint',
-               'ruff',
-               'vulture', ]
-
+PY_MOD_LIST = ['bandit','mccabe','mypy','pyflakes','pylint','ruff','vulture',]
 
 def run_lint_tests(file):
+    """
+    Func: Function runs lint tools on Python source file.
+    Args: Python source file.
+    Returns: Nothing.
+    """
     for py_mod in PY_MOD_LIST:
 
-        # check if file exists
         has_file = os.path.isfile(file)
         if not has_file:
             print(":: ")
@@ -77,10 +81,10 @@ def run_lint_tests(file):
         print(f'  ::> {py_mod} on file: {file}')
         print("  ::>")
 
-        result = run(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
-        result_len = len(result.stdout)
+        result = run(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True,
+                          shell=True, check=False)
 
-        if result_len == 0:
+        if len(result.stdout) == 0:
             print("  ::> ")
             print("  ::> [\nNo problems detected.", "\n  ::> ]")
             print("  ::> ")
@@ -89,8 +93,13 @@ def run_lint_tests(file):
             print("  ::> [\n", result.stdout, "\n  ::> ]")
             print("  ::> ")
 
-
 def check_modules():
+    """
+    Func: The check_modules function checks, installs the lint tools.
+    Args: None.
+    Returns: Nothing.
+    """
+
     print("#")
     print("# Python Linter Tool Script ", __release__)
     print("# ")
@@ -99,6 +108,7 @@ def check_modules():
 
     for package in PY_MOD_LIST:
 
+        print("   ")
         has_mod = importlib.util.find_spec(package)
 
         if has_mod:
@@ -106,59 +116,61 @@ def check_modules():
         else:
             print(f'  #:>The {package} module absent.')
 
-            python = sys.executable
-            subprocess.check_call(
-                [python, '-m', 'pip', 'install', package],
-                stdout=subprocess.DEVNULL
-            )
+            tmp_argv = sys.argv
+            sys.argv = ["pip", "install"] + [ package ]
 
+            try:
+                run_module("pip", run_name="__main__")
+            except SystemExit as exception:
+                print("exit code: ", exception.code)
+                if exception.code != 0 :
+                    print(":: ")
+                    print(f':: Error: The {package} module NOT installed; end tests!')
+                    print(":: ")
+                    sys.exit(2)
+
+            sys.argv = tmp_argv
+            print("  #:> ")
             print(f'  #:>The {package} module installed.')
-
-    pass  # for
+            print("  #:> ")
 
     print("")
     input("\nPress any key to continue...\n")
     print("")
 
-
-pass  # end check_modules
-
-
 def usage():
+    """
+    Func: Usage function prints command-line interface parameters.
+    Args: None.
+    Returns: Nothing.
+    """
+
     print("")
-    print("Usage:", sys.argv[0], "{ file.py } | --help")
+    print("Usage:", sys.argv[0], "{ file.py } | --help | -h")
     print("      ", sys.argv[0], "myPython.py myApp.py badCode.py")
     print("")
 
-
 def version():
+    """
+    Func: Version function prints copyright, release version of tool.
+    Args: None.
+    Returns: Nothing.
+    """
     print("")
-    print("Py7 Lint (C) Copyright ", __copyright__, " Release:", __release__)
+    print("Py7 Lint Tool ©", __copyright__, "Release:", __release__)
     print("")
-    # print("Py7 Lint Written by", __author__,"(",__email__,")")
-    # print("         and maintained by", __maintainer__)
-    # print("")
-    # print("Credits to", __credits__)
-    # print("")
-    # print("License:", __license__, __version__)
-
-
-# check for command-line interface args
-#
-
-# print('\n')
-# print('Number of arguments:', len(sys.argv), 'arguments.')
-# print('Argument List:', str(sys.argv))
-# print("Name of Python script:", sys.argv[0])
 
 def main():
-    # if len(sys.argv) <= 1:
+    """
+    Func: Main function that runs script, lint tools.
+    Args: None.
+    Returns: Nothing.
+    """
     print("")
     version()
     print("")
-    # sys.exit(1)
 
-    if '--help' in sys.argv:
+    if ("-h",'--help') in sys.argv:
         usage()
         sys.exit(0)
 
@@ -169,50 +181,24 @@ def main():
         usage()
         sys.exit(1)
 
-    # else:
-    #    pass
-    #     n = len(sys.argv)
-    #     print("\nArguments passed:")
-    #     for i in range(1, n):
-    #         print(sys.argv[i])
-    #     print()
-    pass  # end if
-
     check_modules()  # check for and install required modules
 
     print("::")
     print(":: Beginning Py7 Lint")
     print("::")
+    print("   ")
 
-    # for i in range(1, n):
-    #    print(sys.argv[i])
-
-    # os.system('pylint main.py')
-
-    # cmd = PY_MOD_LIST[4] + ' ' + 'main.py'
-
-    # os.system(cmd)
-    # result = run(cmd, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
-    # print("code ", result.returncode)
-    # print(result.stdout)
-
-    pass
-
-    n = len(sys.argv)
-
-    for i in range(1, n):
+    for i in range(1, len(sys.argv)):
+        print("\n::--------------------------------------------------------------------------------::\n")
         run_lint_tests(sys.argv[i])
+        print("   ")
 
     print("::")
     print(":: Completed Py7 Lint")
     print("::")
 
     input("\nPress any key to continue...\n")
-
     sys.exit(0)
-
-
-pass  # end main
 
 if __name__ == '__main__':
     main()
