71b17fd638391b414df045055d8522aa0dafe706
William F. Gilreath <will.gilreath@outlook.com>
2025-04-09 23:44:45 -0700

    Add files via upload
    Upload Python source code scripts for use with testing py7.py lint tool.

---
+++ test_code/tipy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#
+
+# Tiny Interpreter PYthon: TIpy.py
+# https://python.plainenglish.io/python-interpreter-with-2-lines-of-code-29ab1f977900 
+# Apr 10 2025
+
+while True:
+    exec(input('>>> '))
+
